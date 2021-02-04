2584758cbbb7876d96c3e76e8853ff12cb4b6ad1
dpuentes <dipuentes123@gmail.com>
2021-02-04 15:14:01 -0500

    correction sintax to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'home' => 'statics_pages#home'
-
-  get 'basics' => 'statics_pages#basics'
-
-  get 'ruby' => 'statics_pages#ruby'
-
-  get 'git' => 'statics_pages#git'
-
   root 'application#hello'
+
+  get 'home', to: 'static_pages#home'
+  get 'basics', to: 'static_pages#basics'
+  get 'ruby', to: 'static_pages#ruby'
+  get 'git', to: 'static_pages#git'
+
 end
